6e5a00427af3b539476a9e4d9dd124839b297297
Adrian <adriancostasgarcia1998@gmail.com>
2024-11-21 14:01:45 +0100

    Ejercicio 11 Completado

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -1,11 +1,27 @@
 package com.campusdual.classroom;
 
+import java.util.Set;
+
 public class Exercise11 {
     public static void main(String[] args) {
         TVRemote redRemote = new TVRemote("Rojo");
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+
+        redRemote.turnOn();
+        redRemote.channelUp();
+        redRemote.channelDown();
+        redRemote.channel = 0;
+        redRemote.channelDown();
+        blackRemote.volumeUp();
+        blackRemote.volumeDown();
+        blackRemote.volume = 0;
+        blackRemote.volumeDown();
+        System.out.println(redRemote.getColor());
+        blackRemote.turnOff();
+
+
     }
 
 }
